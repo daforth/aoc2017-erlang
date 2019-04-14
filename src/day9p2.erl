0c71a66db88b491341511fd3155a436f958095a4
Daniele Parisi <daniele@parisi.xyz>
2019-04-14 18:12:51 +0200

    day9p2

---
+++ src/day9p2.erl
@@ -0,0 +1,20 @@
+-module(day9p2).
+-compile(export_all).
+
+day9p2([$!, _ | Rest], State, Sum) ->
+    day9p2(Rest, State, Sum);
+day9p2([$> | Rest], garbage, Sum) ->
+    day9p2(Rest, normal, Sum);
+day9p2([_ | Rest], garbage, Sum) ->
+    day9p2(Rest, garbage, Sum + 1);
+day9p2([$< | Rest], normal, Sum) ->
+    day9p2(Rest, garbage, Sum);
+day9p2([_ | Rest], normal, Sum) ->
+    day9p2(Rest, normal, Sum);
+day9p2([], normal, Sum) ->
+    Sum.
+
+main(_) ->
+    {ok, Bin} = file:read_file("../input9"),
+    String = binary_to_list(Bin),
+    day9p2(String, normal, 0).
