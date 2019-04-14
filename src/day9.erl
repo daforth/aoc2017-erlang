40dfc2fa7608b21c2bb8e04c0fdd3449047bb210
Daniele Parisi <daniele@parisi.xyz>
2019-04-14 18:15:22 +0200

    style

--- src/day9.erl
+++ src/day9.erl
@@ -1,22 +1,14 @@
 -module(day9).
 -compile(export_all).
 
-day9([$!, _ | Rest], State, Level, Sum) ->
-    day9(Rest, State, Level, Sum);
-day9([$> | Rest], garbage, Level, Sum) ->
-    day9(Rest, normal, Level, Sum);
-day9([_ | Rest], garbage, Level, Sum) ->
-    day9(Rest, garbage, Level, Sum);
-day9([$< | Rest], normal, Level, Sum) ->
-    day9(Rest, garbage, Level, Sum);
-day9([${ | Rest], normal, Level, Sum) ->
-    day9(Rest, normal, Level + 1, Sum);
-day9([$} | Rest], normal, Level, Sum ) ->
-    day9(Rest, normal, Level - 1, Sum + Level);
-day9([_ | Rest], normal, Level, Sum) ->
-    day9(Rest, normal, Level, Sum);
-day9([], normal, 0, Sum) ->
-    Sum.
+day9([$!, _ | Rest], State, Level, Sum) -> day9(Rest, State, Level, Sum);
+day9([$> | Rest], garbage, Level, Sum) -> day9(Rest, normal, Level, Sum);
+day9([_ | Rest], garbage, Level, Sum) -> day9(Rest, garbage, Level, Sum);
+day9([$< | Rest], normal, Level, Sum) -> day9(Rest, garbage, Level, Sum);
+day9([${ | Rest], normal, Level, Sum) -> day9(Rest, normal, Level + 1, Sum);
+day9([$} | Rest], normal, Level, Sum ) -> day9(Rest, normal, Level - 1, Sum + Level);
+day9([_ | Rest], normal, Level, Sum) -> day9(Rest, normal, Level, Sum);
+day9([], normal, 0, Sum) -> Sum.
 
 main(_) ->
     {ok, Bin} = file:read_file("../input9"),
--- src/day9p2.erl
+++ src/day9p2.erl
@@ -1,18 +1,12 @@
 -module(day9p2).
 -compile(export_all).
 
-day9p2([$!, _ | Rest], State, Sum) ->
-    day9p2(Rest, State, Sum);
-day9p2([$> | Rest], garbage, Sum) ->
-    day9p2(Rest, normal, Sum);
-day9p2([_ | Rest], garbage, Sum) ->
-    day9p2(Rest, garbage, Sum + 1);
-day9p2([$< | Rest], normal, Sum) ->
-    day9p2(Rest, garbage, Sum);
-day9p2([_ | Rest], normal, Sum) ->
-    day9p2(Rest, normal, Sum);
-day9p2([], normal, Sum) ->
-    Sum.
+day9p2([$!, _ | Rest], State, Sum) -> day9p2(Rest, State, Sum);
+day9p2([$> | Rest], garbage, Sum) -> day9p2(Rest, normal, Sum);
+day9p2([_ | Rest], garbage, Sum) -> day9p2(Rest, garbage, Sum + 1);
+day9p2([$< | Rest], normal, Sum) -> day9p2(Rest, garbage, Sum);
+day9p2([_ | Rest], normal, Sum) -> day9p2(Rest, normal, Sum);
+day9p2([], normal, Sum) -> Sum.
 
 main(_) ->
     {ok, Bin} = file:read_file("../input9"),
